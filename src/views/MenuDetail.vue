cdbaaae234066ed18e79a09ed6728b082b975c3c
unknown <dgloria.web@gmail.com>
2021-01-13 08:02:05 +0000

    not working

--- src/views/MenuDetail.vue
+++ src/views/MenuDetail.vue
@@ -1,28 +1,29 @@
 <template>
-    <div>
-        {{meals}}
-    </div>
+  <div>
+    <!-- {{meals}} -->
+    <p>The meal name is: {{ meals.name }}</p>
+  </div>
 </template>
 
 <script>
-    export default {
-         data() {
+export default {
+  data() {
     return {
-      meals: {}
+      meals: {},
     };
   },
-     created: function() {
+  created: function () {
     this.fetchData();
   },
 
-    computed: {
+  computed: {
     styles() {
-      return {
-      };
-    }
+      return {};
+    },
+   
   },
   methods: {
-    fetchData: async function() {
+    fetchData: async function () {
       try {
         const res = await fetch(
           `https://dev.menu.ninja/api/menu/156?key=8j5vfe%24*pfb**rzt&pretty=1`
@@ -32,11 +33,10 @@
       } catch (e) {
         console.log(e);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
